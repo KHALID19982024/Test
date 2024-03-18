cfc7f50b29a095096f9b175f2a2fd8e2e43d9254
KHALID185 <KHALID.1998.TAOUTI@GMAIL.COM>
2024-03-18 20:39:47 +0000

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int a = 4;
+        int b = 8;
+        System.out.println("somme "+(a+b));
+    }
+}
